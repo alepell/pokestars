ab0ffff6b041bf6d2a2ad3aee8b61fbfd8fd853a
Alexandre <alesirius@gmail.com>
2019-09-14 19:20:32 -0300

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$("#1 img").click(() => {
+  $("#1 ul").slideToggle(300);
+});
+
+$("#2 img").click(() => {
+  $("#2 ul").slideToggle(300);
+});
+
+$("#3 img").click(() => {
+  $("#3 ul").slideToggle(300);
+});
+
+$("#4 img").click(() => {
+  $("#4 ul").slideToggle(300);
+});
+
+$("#5 img").click(() => {
+  $("#5 ul").slideToggle(300);
+});
