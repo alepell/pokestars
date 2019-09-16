66fcb73ef4207d78c92e757be2fbcb6dde1f8908
Alexandre <alesirius@gmail.com>
2019-09-15 23:59:36 -0300

    subindo styles.css e script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,36 @@ $("#4 img").click(() => {
   $("#4 ul").slideToggle(300);
 });
 
-$("#5 img").click(() => {
+$("#5 img")
+  .first()
+  .click(() => {
+    $("#5 ul").slideToggle(300);
+  });
+
+$("#6 img").click(() => {
+  $("#5 ul").slideToggle(300);
+});
+
+$("#7 img").click(() => {
+  $("#5 ul").slideToggle(300);
+});
+
+$("#8 img").click(() => {
+  $("#5 ul").slideToggle(300);
+});
+
+$("#9 img").click(() => {
+  $("#5 ul").slideToggle(300);
+});
+
+$("#10 img").click(() => {
+  $("#5 ul").slideToggle(300);
+});
+
+$("#11 img").click(() => {
+  $("#5 ul").slideToggle(300);
+});
+
+$("#12 img").click(() => {
   $("#5 ul").slideToggle(300);
 });
