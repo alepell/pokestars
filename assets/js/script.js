2be227b694343c7308d5597a0525d3e92c822c62
Comstock <vitor1correia@gmail.com>
2019-11-02 14:12:05 -0300

    Update na arquitetura dos arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,36 +14,6 @@ $("#4 img").click(() => {
   $("#4 ul").slideToggle(300);
 });
 
-$("#5 img")
-  .first()
-  .click(() => {
-    $("#5 ul").slideToggle(300);
-  });
-
-$("#6 img").click(() => {
-  $("#6 ul").slideToggle(300);
-});
-
-$("#7 img").click(() => {
-  $("#7 ul").slideToggle(300);
-});
-
-$("#8 img").click(() => {
-  $("#8 ul").slideToggle(300);
-});
-
-$("#9 img").click(() => {
-  $("#9 ul").slideToggle(300);
-});
-
-$("#10 img").click(() => {
-  $("#10 ul").slideToggle(300);
-});
-
-$("#11 img").click(() => {
-  $("#11 ul").slideToggle(300);
-});
-
-$("#12 img").click(() => {
-  $("#12 ul").slideToggle(300);
+$("#5 img").click(() => {
+  $("#5 ul").slideToggle(300);
 });
